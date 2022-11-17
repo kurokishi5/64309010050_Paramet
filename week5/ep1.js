91953c7d885cb9038fb9cd2ebfaba0698e43b2ab
kurokishi5 <116155883+kurokishi5@users.noreply.github.com>
2022-11-17 09:29:13 +0700

    Create ep1.js

---
+++ week5/ep1.js
@@ -0,0 +1,12 @@
+let order = {
+     'A4paper' : 120,
+     'Ruler' : 25,
+     'FlashDrive' : 130,
+     'A5paper' : 125,
+}
+
+console.log('Mr.Lee', order.A4paper * 5 + order.Ruler * 2)
+
+console.log('Mr.Yue', order.FlashDrive * 2 + order.A4paper * 3)
+
+console.log('Mr.Tae', order.A5paper * 5 + order.Ruler * 3)
