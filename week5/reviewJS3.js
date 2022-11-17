adbec7dcb630f8646ca3a623800f75f1555fd829
kurokishi5 <116155883+kurokishi5@users.noreply.github.com>
2022-11-17 09:30:14 +0700

    Create reviewJS3.js

---
+++ week5/reviewJS3.js
@@ -0,0 +1,8 @@
+let obj = {
+    'studentID': 64309010050,
+    'name':'kuro',
+    'lastname' : 'kishi',
+    'grade': 3.35,
+    'Tel':[0650455131,0938425020]
+}
+console.log(obj.name,obj.lastname, obj.grade, obj.Tel[0])
