c81a4db4b15e967a303b6b9d64a1961bd2cd7871
kurokishi5 <116155883+kurokishi5@users.noreply.github.com>
2022-12-07 15:25:57 +0700

    Create w7.js

---
+++ week6/w7.js
@@ -0,0 +1,77 @@
+let employee = [{
+    "ID" : 3001,
+    "name" :"นางปรียาดา ตั้งทิพย์" ,
+    "department" : "บุคคล" ,
+    "position" : "HR" ,
+    "salary" : 48000 ,
+    "bonusRank" : "A"
+},{
+    "ID" : 3002,
+    "name" :"นายณัฏฐ์ พงษ์วิโรจน์" ,
+    "department" : "ไอที" ,
+    "position" : "System Engineer" ,
+    "salary" : 57000 ,
+    "bonusRank" : "S"
+},{
+    "ID" : 3003,
+    "name" :"นายภูวนัย พัฒนปรีชา" ,
+    "department" : "การเงิน" ,
+    "position" : "Account" ,
+    "salary" : 38000 ,
+    "bonusRank" : "B"
+},{
+    "ID" : 3004,
+    "name" :"นางสาวเจนจิรา วงศ์สุรวัฒนา" ,
+    "department" : "บุคคล" ,
+    "position" : "HR" ,
+    "salary" : 55000 ,
+    "bonusRank" : "A" ,
+},{
+    "ID" : 3005,
+    "name" :"นายพนาวัฒน์ กลิ่นโพธิ์" ,
+    "department" : "บุคคล" ,
+    "position" : "HR" ,
+    "salary" : 13000 ,
+    "bonusRank" : "C" ,
+},{
+    "ID" : 3006,
+    "name" :"นางธนารีย์ ชัยเจริญ" ,
+    "department" : "การเงิน" ,
+    "position" : "Account" ,
+    "salary" : 41000 ,
+    "bonusRank" : "S"
+},{
+    "ID" : 3007,
+    "name" :"นายนิมมาน สยนานนท์" ,
+    "department" : "บุคคล" ,
+    "position" : "HR" ,
+    "salary" : 19000 ,
+    "bonusRank" : "A"
+},{
+    "ID" : 3008,
+    "name" :"นายกีรติ มากมี" ,
+    "department" : "การเงิน" ,
+    "position" : "Account" ,
+    "salary" : 46000 ,
+    "bonusRank" : "C"
+},{
+    "ID" : 3009,
+    "name" :"นางสาววรรณิศา โตศิลา" ,
+    "department" : "ไอที" ,
+    "position" : "System Engineer" ,
+    "salary" : 28000 ,
+    "bonusRank" : "B"
+}]
+let bonusratinglevel = [{
+    "level" : "S",
+    "bonus" :1.8
+},{
+    "level" : "A",
+    "bonus" :1.4
+},{
+    "level" : "B",
+    "bonus" :1.15
+},{
+    "level" : "C",
+    "bonus" :8.0
+}]
