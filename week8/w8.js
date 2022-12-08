b3cc4b63f4869f062e9360989ab7e622bc8ee02c
kurokishi5 <116155883+kurokishi5@users.noreply.github.com>
2022-12-08 09:46:23 +0700

    Create w8.js

---
+++ week8/w8.js
@@ -0,0 +1,11 @@
+let json = '{"fname":"sam","Inam":"Samth"}';
+let obj = JSON.parse(JSON);
+
+console.log(typeof(json));
+
+let obj2 = {
+    fname : "jack",
+    Iname : "Samth"
+};
+let json2 = JSON.parse(JSON);
+console.log(typeof(json));
