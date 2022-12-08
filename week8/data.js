18e24aa1d03b00473744f3a95641e1898b1bb1f3
kurokishi5 <116155883+kurokishi5@users.noreply.github.com>
2022-12-08 09:50:14 +0700

    Update data.js

--- week8/data.js
+++ week8/data.js
@@ -1,3 +1,11 @@
-{
-    key:"String"
-}
+let json = '{"fname":"sam","Inam":"Samth"}';
+let obj = JSON.parse(JSON);
+
+console.log(typeof(json));
+
+let obj2 = {
+    fname : "jack",
+    Iname : "Samth"
+};
+let json2 = JSON.parse(JSON);
+console.log(typeof(json));
